014e64c586173ac1c618283900bea4fc15843269
drunkcoder1984 <drunkcoder1984@gmail.com>
2024-09-28 16:46:49 +0900

    actorのhello world

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+
+	console "github.com/asynkron/goconsole"
+	"github.com/asynkron/protoactor-go/actor"
+)
+
+type (
+	hello      struct{ Who string }
+	helloActor struct{}
+)
+
+func (state *helloActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *hello:
+		context.Logger().Info("Hello ", slog.String("who", msg.Who))
+	}
+}
 
 func main() {
-	fmt.Println("hello world!")
+	system := actor.NewActorSystem()
+	props := actor.PropsFromProducer(func() actor.Actor { return &helloActor{} })
+
+	pid := system.Root.Spawn(props)
+	system.Root.Send(pid, &hello{Who: "Roger"})
+	_, _ = console.ReadLine()
 }
